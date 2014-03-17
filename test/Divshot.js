83e4cc10b958e96589c6caa95b712aaf3f976e04
Scott Corgan <scott@divshot.com>
2014-03-17 12:06:42 -0700

    Testing with custom runner

--- test/Divshot.js
+++ test/Divshot.js
@@ -1,76 +1,263 @@
 var Divshot = require('../lib/divshot');
-var lab = require('lab');
-var describe = lab.describe;
-var it = lab.it;
-var expect = lab.expect;
-var beforeEach = lab.beforeEach;
-var afterEach = lab.afterEach;
+var tape = require('tape');
+var async = require('async');
+var extend = require('extend');
 
-describe('divshot api wrapper set up', function () {
-  var divshot;
+
+
+
+var suite = function (name, setupRunner, options) {
+  var endNoop = function (t) {t.end();};
+  var afterEach = [];
+  var beforeEach = [];
+  var tests = tests || [];
   
-  beforeEach(function (done) {
-    divshot = new Divshot();
-    done();
+  // Transfer options to nested suites
+  if (options) {
+    beforeEach = options.beforeEach || [endNoop];
+    afterEach = options.afterEach || [endNoop];
+  }
+  
+  tape(name, function (t) {
+    var originalTest = t.test.bind(t);
+    
+    t.beforeEach = function (fn) {
+      beforeEach.push(fn);
+    };
+    
+    t.afterEach = function (fn) {
+      afterEach.push(fn);
+    };
+    
+    t.test = function (name, fn) {
+      tests.push({
+        name: name,
+        fn: fn
+      });
+    };
+    
+    t.suite = function (name, setupRunner) {
+      suite(name, setupRunner, extend(options, {
+        beforeEach: beforeEach,
+        afterEach: afterEach
+      }));
+    };
+    
+    // Run it all
+    setupRunner(t);    
+    process.nextTick(runTests);
+    
+    function runTests () {
+      async.eachSeries(tests, function (testItem, done) {
+        
+        runBeforeEach(runTest);
+        
+        function runTest () {
+          originalTest(testItem.name, function (runner) {
+            var originalEnd = runner.end.bind(runner);
+            
+            runner.end = function () {
+              runAfterEach(function () {
+                done();
+                originalEnd();
+              });
+            };
+            
+            testItem.fn(runner);
+          });
+        }
+      }, t.end.bind(t));
+      
+      function runBeforeEach (done) {
+        async.eachSeries(beforeEach, function (before, done) {
+          before({end: done})
+        }, done);
+      }
+      
+      function runAfterEach (done) {
+        async.eachSeries(afterEach, function (after, done) {
+          after({end: done})
+        }, done);
+      }
+    }
   });
   
-  it('sets the token', function (done) {
-    divshot.token('token');
-    expect(divshot.token()).to.equal('token');
-    done();
+};
+
+
+
+
+suite('something', function (t) {
+  
+  t.beforeEach(function (t) {
+    console.log('===================before each=======================');
+    t.end();
   });
   
-  it('sets the token header when the token is set', function (done) {
-    divshot.token('token');
-    expect(divshot.header('Authorization')).to.equal('Bearer token');
-    done();
+  t.test('does something', function (t) {
+    t.ok(true, 'test 1');
+    t.end();
   });
   
-  it('overrides the host', function (done) {
-    divshot.host('host');
-    expect(divshot.host()).to.equal('host');
-    done();
+  t.test('does somethiasdfasng', function (t) {
+    t.ok(true, 'test 2');
+    t.end();
   });
   
-  it('sets the client id on the session header', function (done) {
-    divshot.session('client_id');
-    expect(divshot.header('Authorization')).to.equal('Session client_id');
-    expect(divshot.clientId()).to.equal('client_id');
-    done();
-  });
-  
-  it('forces CORS with credentials if session is set', function (done) {
-    divshot.session('client_id');
-    expect(divshot.xhr('withCredentials')).to.equal(true);
-    done();
-  });
-  
-  it('has a default api version of 0.5.0', function (done) {
-    expect(divshot.apiVersion()).to.equal('0.5.0');
-    done();
-  });
-  
-  it('sets a custom api version', function (done) {
-    divshot.apiVersion('0.6.0');
-    expect(divshot.apiVersion()).to.equal('0.6.0');
-    done();
-  });
-  
-  it('sets the default api version from the environment', function (done) {
-    process.env.DIVSHOT_API_URL = '0.6.0';
-    var divshot = new Divshot();
-    expect(divshot.apiVersion()).to.equal('0.6.0');
-    done();
-  });
-  
-  it('sets the version header for all http requests', function (done) {
-    expect(divshot.header('Accepts-Version')).to.equal(divshot.apiVersion());
-    done();
+  t.suite('another thing', function (t) {
+    
+    t.beforeEach(function (t) {
+      console.log('===================NESTED: before each=======================');
+      t.end();
+    });
+    
+    t.afterEach(function (t) {
+      console.log('===================NESTED: after each=======================');
+      t.end();
+    });
+    
+    t.test('testing this', function (t) {
+      t.ok(true);
+      t.end();
+    });
+    
+    t.suite('double nested', function (t) {
+      
+      t.beforeEach(function (t) {
+        console.log('===================DOUBLE NESTED: before each=======================');
+        t.end();
+      });
+      
+      t.afterEach(function (t) {
+        console.log('===================DOUBLE NESTED: after each=======================');
+        t.end();
+      });
+      
+      t.test('nested testing', function (t) {
+        t.ok(true);
+        t.end();
+      });
+    });
+    
+    t.test('nested testing this ya', function (t) {
+      t.ok(true);
+      t.end();
+    });
   });
   
 });
 
-function requireNoCache (pathname) {
-  delete require.cache[pathname];
-  return require(pathname);
-}
+
+
+
+
+
+
+// test('divshot api wrapper set up', function (t) {
+//   var divshot;
+  
+//   t.beforeEach = function (fn) {
+//     fn(function () {
+//       divshot.token('token');
+//       t.equal(divshot.token(), 'token', 'set the token');
+//       t.end();
+//     });
+//   }
+  
+//   t.beforeEach(function (done) {
+//     divshot = new Divshot();
+//     console.log('beforeEach')
+//     done();
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var lab = require('lab');
+// var describe = lab.describe;
+// var it = lab.it;
+// var expect = lab.expect;
+// var beforeEach = lab.beforeEach;
+// var afterEach = lab.afterEach;
+
+// describe('divshot api wrapper set up', function () {
+//   var divshot;
+  
+//   beforeEach(function (done) {
+//     divshot = new Divshot();
+//     done();
+//   });
+  
+//   it('sets the token', function (done) {
+//     divshot.token('token');
+//     expect(divshot.token()).to.equal('token');
+//     done();
+//   });
+  
+//   it('sets the token header when the token is set', function (done) {
+//     divshot.token('token');
+//     expect(divshot.header('Authorization')).to.equal('Bearer token');
+//     done();
+//   });
+  
+//   it('overrides the host', function (done) {
+//     divshot.host('host');
+//     expect(divshot.host()).to.equal('host');
+//     done();
+//   });
+  
+//   it('sets the client id on the session header', function (done) {
+//     divshot.session('client_id');
+//     expect(divshot.header('Authorization')).to.equal('Session client_id');
+//     expect(divshot.clientId()).to.equal('client_id');
+//     done();
+//   });
+  
+//   it('forces CORS with credentials if session is set', function (done) {
+//     divshot.session('client_id');
+//     expect(divshot.xhr('withCredentials')).to.equal(true);
+//     done();
+//   });
+  
+//   it('has a default api version of 0.5.0', function (done) {
+//     expect(divshot.apiVersion()).to.equal('0.5.0');
+//     done();
+//   });
+  
+//   it('sets a custom api version', function (done) {
+//     divshot.apiVersion('0.6.0');
+//     expect(divshot.apiVersion()).to.equal('0.6.0');
+//     done();
+//   });
+  
+//   it('sets the default api version from the environment', function (done) {
+//     process.env.DIVSHOT_API_URL = '0.6.0';
+//     var divshot = new Divshot();
+//     expect(divshot.apiVersion()).to.equal('0.6.0');
+//     done();
+//   });
+  
+//   it('sets the version header for all http requests', function (done) {
+//     expect(divshot.header('Accepts-Version')).to.equal(divshot.apiVersion());
+//     done();
+//   });
+  
+// });
+
+// function requireNoCache (pathname) {
+//   delete require.cache[pathname];
+//   return require(pathname);
+// }
--- test/apps.js
+++ test/apps.js
@@ -1,120 +1,120 @@
-var Divshot = require('../lib/divshot');
-var Mocksy = require('mocksy');
-var server = new Mocksy({port: 9999});
-var lab = require('lab');
-var describe = lab.describe;
-var it = lab.it;
-var expect = lab.expect;
-var beforeEach = lab.beforeEach;
-var afterEach = lab.afterEach;
+// var Divshot = require('../lib/divshot');
+// var Mocksy = require('mocksy');
+// var server = new Mocksy({port: 9999});
+// var lab = require('lab');
+// var describe = lab.describe;
+// var it = lab.it;
+// var expect = lab.expect;
+// var beforeEach = lab.beforeEach;
+// var afterEach = lab.afterEach;
 
-describe('apps', function () {
-  var divshot;
+// describe('apps', function () {
+//   var divshot;
   
-  beforeEach(function (done) {
-    divshot = new Divshot();
-    divshot.host('http://localhost:9999');
-    server.start(done);
-  });
+//   beforeEach(function (done) {
+//     divshot = new Divshot();
+//     divshot.host('http://localhost:9999');
+//     server.start(done);
+//   });
   
-  afterEach(function (done) {
-    server.stop(done);
-  });
+//   afterEach(function (done) {
+//     server.stop(done);
+//   });
   
-  it('gets a list of apps', function (done) {
-    divshot.apps.list().then(function (res) {
-      expect(res.url).to.equal('/apps');
-      expect(res.method).to.equal('GET');
-      done();
-    });
-  });
+//   it('gets a list of apps', function (done) {
+//     divshot.apps.list().then(function (res) {
+//       expect(res.url).to.equal('/apps');
+//       expect(res.method).to.equal('GET');
+//       done();
+//     });
+//   });
   
-  it('gets an app by id', function (done) {
-    divshot.apps.id(123).get().then(function (res) {
-      expect(res.url).to.equal('/apps/123');
-      expect(res.method).to.equal('GET');
-      done();
-    });
-  });
+//   it('gets an app by id', function (done) {
+//     divshot.apps.id(123).get().then(function (res) {
+//       expect(res.url).to.equal('/apps/123');
+//       expect(res.method).to.equal('GET');
+//       done();
+//     });
+//   });
   
-  describe('single app functions', function () {
-    var app;
+//   describe('single app functions', function () {
+//     var app;
     
-    beforeEach(function (done) {
-      app = divshot.apps.id(123);
-      server.start(done);
-    });
+//     beforeEach(function (done) {
+//       app = divshot.apps.id(123);
+//       server.start(done);
+//     });
     
-    afterEach(function (done) {
-      server.stop(done);
-    });
+//     afterEach(function (done) {
+//       server.stop(done);
+//     });
     
-    // domains
+//     // domains
     
-    it('gets a list of domains for a given app id', function (done) {
-      app.domains.list().then(function (res) {
-        expect(res.url).to.equal('/apps/123/domains');
-        done();
-      });
-    });
+//     it('gets a list of domains for a given app id', function (done) {
+//       app.domains.list().then(function (res) {
+//         expect(res.url).to.equal('/apps/123/domains');
+//         done();
+//       });
+//     });
     
-    it('adds a domain', function (done) {
-      app.domains.add('www.divshot.com').then(function (res) {
-        expect(res.url).to.equal('/apps/123/domains/www.divshot.com');
-        expect(res.method).to.equal('PUT');
-        done();
-      });
-    });
+//     it('adds a domain', function (done) {
+//       app.domains.add('www.divshot.com').then(function (res) {
+//         expect(res.url).to.equal('/apps/123/domains/www.divshot.com');
+//         expect(res.method).to.equal('PUT');
+//         done();
+//       });
+//     });
     
-    it('removes a domain', function (done) {
-      app.domains.remove('www.divshot.com').then(function (res) {
-        expect(res.url).to.equal('/apps/123/domains/www.divshot.com');
-        expect(res.method).to.equal('DELETE');
-        done();
-      });
-    });
+//     it('removes a domain', function (done) {
+//       app.domains.remove('www.divshot.com').then(function (res) {
+//         expect(res.url).to.equal('/apps/123/domains/www.divshot.com');
+//         expect(res.method).to.equal('DELETE');
+//         done();
+//       });
+//     });
     
-    // environments
+//     // environments
     
-    it('updates an apps configuration', function (done) {
-      app.env('production').configure({
-        name: 'name'
-      }).then(function (res) {
-        expect(res.url).to.equal('/apps/123/env/production/config');
-        expect(res.body).to.eql({name: 'name'});
-        expect(res.method).to.equal('PUT');
-        done();
-      });
-    });
+//     it('updates an apps configuration', function (done) {
+//       app.env('production').configure({
+//         name: 'name'
+//       }).then(function (res) {
+//         expect(res.url).to.equal('/apps/123/env/production/config');
+//         expect(res.body).to.eql({name: 'name'});
+//         expect(res.method).to.equal('PUT');
+//         done();
+//       });
+//     });
     
-    // builds
+//     // builds
     
-    it('gets a single build by id', function (done) {
-      app.builds.id(456).get().then(function (res) {
-        expect(res.url).to.equal('/apps/123/builds/456');
-        done();
-      });
-    });
+//     it('gets a single build by id', function (done) {
+//       app.builds.id(456).get().then(function (res) {
+//         expect(res.url).to.equal('/apps/123/builds/456');
+//         done();
+//       });
+//     });
     
-    it('finalizes a build for a given app', function (done) {
-      app.builds.finalize(456).then(function (res) {
-        expect(res.url).to.equal('/apps/123/builds/456/finalize');
-        done();
-      });
-    });
+//     it('finalizes a build for a given app', function (done) {
+//       app.builds.finalize(456).then(function (res) {
+//         expect(res.url).to.equal('/apps/123/builds/456/finalize');
+//         done();
+//       });
+//     });
     
-    it('releases an environment for a given app', function (done) {
-      app.builds.id(789).release('production').then(function (res) {
-        expect(res.url).to.equal('/apps/123/releases/production');
-        expect(res.method).to.equal('POST');
-        expect(res.body).to.eql({build: '789'});
-        done();
-      });
-    });
+//     it('releases an environment for a given app', function (done) {
+//       app.builds.id(789).release('production').then(function (res) {
+//         expect(res.url).to.equal('/apps/123/releases/production');
+//         expect(res.method).to.equal('POST');
+//         expect(res.body).to.eql({build: '789'});
+//         done();
+//       });
+//     });
     
-    // releases
+//     // releases
     
-  });
+//   });
   
   
-});
+// });

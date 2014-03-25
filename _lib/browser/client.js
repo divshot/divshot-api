c01333978f25e9564125c4c911404a0db6d2dae5
Scott Corgan <scott@divshot.com>
2014-03-25 06:31:41 -0700

    Remove old files

--- _lib/browser/client.js
+++
@@ -1,7 +0,0 @@
-var Divshot = require('../Divshot.js');
-var auth = require('./auth.js');
-
-Divshot.prototype.auth = auth.auth;
-Divshot.prototype.authWithCookie = auth.authWithCookie;
-
-module.exports = Divshot;
